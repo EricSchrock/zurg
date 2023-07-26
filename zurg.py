1b6ae96ec0b89bfc9635c24761809a7ab54594ef
Eric Schrock <eric.schrock22@gmail.com>
2023-07-26 12:59:50 -0400

    zurg.py with time limits

--- zurg.py
+++ zurg.py
@@ -1,6 +1,9 @@
 from itertools import combinations
 
-def one_returns(not_across, across, time_limit, accumulator):
+def one_returns(not_across, across, time_spent, time_limit, accumulator):
+    if time_spent > time_limit:
+        return []
+
     if not_across == []:
         return [accumulator]
 
@@ -8,13 +11,17 @@ def one_returns(not_across, across, time_limit, accumulator):
     for toy in across:
         solutions += two_cross(not_across  = not_across + [toy],
                                across      = [x for x in across if x is not toy],
+                               time_spent  = time_spent + toy,
                                time_limit  = time_limit,
                                accumulator = accumulator + [toy])
 
     return solutions
 
 
-def two_cross(not_across, across, time_limit, accumulator):
+def two_cross(not_across, across, time_spent, time_limit, accumulator):
+    if time_spent > time_limit:
+        return []
+
     pairs = list(combinations(not_across, r=2))
 
     solutions = []
@@ -22,6 +29,7 @@ def two_cross(not_across, across, time_limit, accumulator):
         pair = sorted(list(pair))
         solutions += one_returns(not_across  = [x for x in not_across if x not in pair],
                                  across      = across + pair,
+                                 time_spent  = time_spent + max(pair),
                                  time_limit  = time_limit,
                                  accumulator = accumulator + [pair])
 
@@ -29,11 +37,11 @@ def two_cross(not_across, across, time_limit, accumulator):
 
 
 def zurg():
-    not_across = [5,10,20,25]
-    across = []
-    time_limit = 60
-
-    return two_cross(not_across, across, time_limit, accumulator=[])
+    return two_cross(not_across  = [5,10,20,25],
+                     across      = [],
+                     time_spent  = 0,
+                     time_limit  = 60,
+                     accumulator = [])
 
 
 if __name__ == "__main__":

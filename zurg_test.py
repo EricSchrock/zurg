64f805a5e4527c5d42a6cd1e6537651099cd1f74
Eric Schrock <eric.schrock22@gmail.com>
2023-07-26 11:54:03 -0400

    Basic zurg.py tests

---
+++ zurg_test.py
@@ -0,0 +1,8 @@
+from zurg import *
+
+def test_zurg():
+    solutions = zurg()
+
+    assert len(solutions) == 2
+    assert [[5,10], 5,[20,25],10,[5,10]] in solutions
+    assert [[5,10],10,[20,25], 5,[5,10]] in solutions

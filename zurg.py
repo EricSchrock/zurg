c4c1eb99af028b541d2ce9186053be5091281ea4
Eric Schrock <eric.schrock22@gmail.com>
2023-07-26 12:10:44 -0400

    zurg.py returning a solution but no time limit implemented yet

--- zurg.py
+++ zurg.py
@@ -1,5 +1,30 @@
+def one_returns_with_flashlight(not_across, across, time_limit, solution):
+    if not_across == []:
+        return solution
+
+    toy = across.pop()
+    not_across.append(toy)
+    solution.append(toy)
+
+    return two_cross_bridge(not_across, across, time_limit, solution)
+
+
+def two_cross_bridge(not_across, across, time_limit, solution):
+    toy1 = not_across.pop()
+    toy2 = not_across.pop()
+
+    across += [toy1,toy2]
+    solution.append([toy1,toy2])
+
+    return one_returns_with_flashlight(not_across, across, time_limit, solution)
+
+
 def zurg():
-    return [[[5,10],5,[20,25],10,[5,10]],[[5,10],10,[20,25],5,[5,10]]]
+    not_across = [5,10,20,25]
+    across = []
+    time_limit = 60
+
+    return two_cross_bridge(not_across, across, time_limit, solution=[])
 
 
 if __name__ == "__main__":
